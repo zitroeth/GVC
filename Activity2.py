721090d2665cb2e90b66fc300e7a638bfa84081a
Ethan <ethan.ortiz.21@usjr.edu.ph>
2024-03-20 08:07:29 +0800

    activity2 done

---
+++ Activity2.py
@@ -0,0 +1,22 @@
+import cv2
+import numpy as np
+
+modifPixVal, modifPixAfter, i = 0, 10, 0
+
+image1path = r'D:\Coding\Python\GVC\Images\image3.png'
+image1 = cv2.imread(image1path, cv2.IMREAD_COLOR)
+
+rows, columns, _ = image1.shape
+
+for x in range(rows):
+    for y in range(columns):
+        if i < modifPixAfter:
+            i+=1
+            continue
+        image1.itemset((x, y, 0), modifPixVal)
+        image1.itemset((x, y, 1), modifPixVal)
+        image1.itemset((x, y, 2), modifPixVal)
+
+cv2.imshow('Image 1', image1)
+cv2.waitKey(0)
+
